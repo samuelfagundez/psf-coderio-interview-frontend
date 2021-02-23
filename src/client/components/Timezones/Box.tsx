f60e1eefe6f3eb8f7be4189b3c344b1ea9ba8f5c
Samuel <samuelfagundez97@gmail.com>
2021-02-23 19:05:24 -0400

    Including flexbox for the timezone boxes

--- src/client/components/Timezones/Box.tsx
+++ src/client/components/Timezones/Box.tsx
@@ -5,18 +5,20 @@ import { Timezone } from '../../../shared/models/Timezone'
 const Box = (props: { timezone: Timezone}) => {
 
     return (
-        <div className="boxContainer">
-            <i className="fa fa-times icon"></i>
-            <div className="boxDataContainer">
-                <h3 className="boxH3">
-                    {props.timezone.name}
-                </h3>
-                <h4 className="boxH4">
-                    {props.timezone.timezone.date}
-                </h4>
-                <h4 className="boxH4">
-                    {props.timezone.timezone.time}
-                </h4>
+        <div className="flex4theBox">
+            <div className="boxContainer">
+                <i className="fa fa-times icon"></i>
+                <div className="boxDataContainer">
+                    <h3 className="boxH3">
+                        {props.timezone.name}
+                    </h3>
+                    <h4 className="boxH4">
+                        {props.timezone.timezone.date}
+                    </h4>
+                    <h4 className="boxH4">
+                        {props.timezone.timezone.time}
+                    </h4>
+                </div>
             </div>
         </div>
     )
--- src/client/pages/Timezones/index.tsx
+++ src/client/pages/Timezones/index.tsx
@@ -7,13 +7,15 @@ import '../../css/Timezones/PageStyles.css'
 const Timezones = () => {
 
     const [timezones, setTimezones] = useState<Timezone[]>([])
-    const [selectedTimezones, setSelectedTimezones] = useState<Timezone[]>([{name: 'micasa', timezone: { date: '15/02/2021', time: '12:32 PM'}}])
+    const [selectedTimezones, setSelectedTimezones] = useState<Timezone[]>([{name: 'micasa', timezone: { date: '15/02/2021', time: '12:32 PM'}}, {name: 'micasa', timezone: { date: '15/02/2021', time: '12:32 PM'}}, {name: 'micasa', timezone: { date: '15/02/2021', time: '12:32 PM'}}, {name: 'micasa', timezone: { date: '15/02/2021', time: '12:32 PM'}}, {name: 'micasa', timezone: { date: '15/02/2021', time: '12:32 PM'}}])
 
     return (
         <>
             <div className="TimezonesContainer">
                 <Searchbar timezones={timezones} />
-                {selectedTimezones.map((e: Timezone, i: number) => <TimezoneBox timezone={e} key={i} /> )}
+                <div className="timezoneBoxesContainer">
+                    {selectedTimezones.map((e: Timezone, i: number) => <TimezoneBox timezone={e} key={i} /> )}
+                </div>
             </div>
         </>
     )
