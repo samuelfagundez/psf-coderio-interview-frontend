2089be0f57e6e05cdc1441886d79a426bd1a3c2a
Samuel <samuelfagundez97@gmail.com>
2021-02-23 20:12:54 -0400

    Filtering timezones

--- src/client/components/Timezones/Searchbar.tsx
+++ src/client/components/Timezones/Searchbar.tsx
@@ -1,15 +1,29 @@
-import React, { Dispatch, SetStateAction } from 'react'
+import React, { Dispatch, SetStateAction, useState } from 'react'
 import '../../css/Timezones/SearchbarStyles.css'
 import { Timezone } from '../../../shared/models/Timezone'
 
 const Searchbar = (props: { timezones: Timezone[], setSelectedTimezones: Dispatch<SetStateAction<Timezone[]>> }) => {
 
-    const [searchTextfield, setSearchTextfield] = React.useState('')
+    const [searchTextfield, setSearchTextfield] = useState<string>('')
+    const [filteredTimezones, setFilteredTimezones] = useState<Timezone[]>([])
+
+    console.log('filterd timezones', filteredTimezones)
+
+    console.log('timezones', props.timezones)
+
 
     return (
         <div className="searchBarContainer">
             <i className="fa fa-search icon"></i> 
-            <input placeholder="Insert a timezone..." className="inputTimezone" value={searchTextfield} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchTextfield(e.target.value)} />
+            <input placeholder="Insert a timezone..." 
+                className="inputTimezone"
+                value={searchTextfield}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    if(e.target.value) setFilteredTimezones(props.timezones.filter((timezone: Timezone) => timezone.name.toLowerCase().includes(e.target.value.toLowerCase())))
+                    else setFilteredTimezones([])
+                    setSearchTextfield(e.target.value)
+                }} 
+            />
         </div>
     )
 }
